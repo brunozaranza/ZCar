e431b30429f719aacc3ea80c197569eba17d8bf7
Bruno Zaranza <edson.sousa@funceme.br>
2018-11-14 17:41:54 -0300

    Initial layout

--- mobile/src/main/java/com/zaranzalabs/zcar/MainActivity.java
+++ mobile/src/main/java/com/zaranzalabs/zcar/MainActivity.java
@@ -2,12 +2,43 @@ package com.zaranzalabs.zcar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.zaranzalabs.zcar.adapter.CellMainAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    private RecyclerView recyclerView;
+
+    private CellMainAdapter rvAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+
+        setupRecycler();
+    }
+
+    private void setupRecycler()
+    {
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            arrayList.add("Cell " + i);
+        }
+
+        rvAdapter = new CellMainAdapter(arrayList);
+        recyclerView.setAdapter(rvAdapter);
+
     }
 }
